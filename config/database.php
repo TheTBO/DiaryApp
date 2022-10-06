c401568041498c72fb2895a1348d330ebae8d491
TheTBO <almujtaba.mhmd@gmail.com>
2022-10-06 21:19:59 +0800

    add mongoDB support

--- config/database.php
+++ config/database.php
@@ -91,6 +91,12 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => env('DB_URI'),
+            'database' => 'diary',
+    ],
+
     ],
 
     /*
