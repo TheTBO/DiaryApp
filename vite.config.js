5cde1d96fad7816309e992a90d3da8b235ff8789
TheTBO <almujtaba.mhmd@gmail.com>
2022-10-07 13:46:50 +0800

    enable vite https support

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import laravel from 'vite-plugin-laravel';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import laravel from "vite-plugin-laravel";
 
 export default defineConfig({
-	plugins: [
-		react(),
-		laravel()
-	],
+    plugins: [react(), laravel()],
+    server: {
+        https: true,
+    },
 });
